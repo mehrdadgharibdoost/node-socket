4162b76fb59ae4cc6d02f806931253a0171b0a34
mehrdad gharibdoost <gharibdoost@hotmail.com>
2019-11-03 17:01:56 +0330

    make room & namespaces

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const io = require("socket.io-client");
 
-let socket = io.connect("http://127.0.0.1:3000");
+let socketTest = io.connect("http://127.0.0.1:3000/test");
 
-socket.on("welcome", (data) => {
+socketTest.on("welcome", (data) => {
     console.log("Received:" + data);
 });
+
+socketTest.emit("joinRoom", "musicians");
+socketTest.on('newUser', (res) => { console.log(res); });
+socketTest.on("err", (err) => { console.log(err); });
+socketTest.on("success", (success) => { console.log(success); });
--- server.js
+++ server.js
@@ -4,9 +4,22 @@ const port = 3000;
 const http = require("http").createServer();
 const io = require("socket.io")(http);
 
-io.on("connection", (socket) => {
+let rooms = ["musicians", "teachers", "singers"]
+
+io.of("/test").on("connection", (socket) => {
     socket.emit("welcome", "hello dear user ...");
     console.log("New Client is Connected ...");
+
+    socket.on("joinRoom", (room) => {
+        if (rooms.includes(room)) {
+            socket.join(room);
+            socket.emit("success", `you joins successfuly in room named: ${room}`);
+            io.of("/test").in(room).emit('newUser', 'new user joined to the room named: ' + room);
+        } else {
+            socket.emit("err", `the room named '${room}' is not exists`);
+        }
+
+    });
 });
 
 http.listen(port, () => {
